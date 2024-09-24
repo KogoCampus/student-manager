64649490d7769c85adc46ac506f6fd74489d2619
Jiin Kim <jiinkim@Jiins-MacBook-Pro.local>
2024-09-23 23:22:29 -0400

    add build script

---
+++ build.js
@@ -0,0 +1,18 @@
+const esbuild = require('esbuild');
+const chalk = require('chalk');
+
+console.log('\x1b[34mBuilding the project...\x1b[0m');
+
+esbuild.build({
+  entryPoints: ['bin/student-manager.ts'],
+  bundle: true,
+  platform: 'node',
+  outfile: 'dist/student-manager.js',
+  external: ['aws-sdk'], // Exclude AWS SDK since it's available in the Lambda runtime
+  sourcemap: true,
+}).then(() => {
+  console.log('\x1b[34mBuild completed successfully.\x1b[0m');
+}).catch(() => {
+  console.error('\x1b[31mBuild failed.\x1b[0m');
+  process.exit(1);
+});
