6ff6c065fb999a73ea59c2bf8c996dc9344d6329
jiin-kim109 <jiin-kim109@users.noreply.github.com>
2024-10-01 03:16:56 +0000

    deploy: 59f25d1941be2490218831fe7b3adee2bda21a74

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    // example: git commit -m "foo: test eslint"
-    // type [foo] is for any neglectable changes
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'ci', 'test', 'foo']],
-  },
-};
