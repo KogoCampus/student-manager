04e5d82c9b78dad091245672cc0a1684647489d4
Jiin Kim <jiinkim@Jiins-MacBook-Pro.local>
2024-10-14 14:20:48 -0400

    foo: attach lambda common sg

--- packages/cdk/lib/lambdaStack.ts
+++ packages/cdk/lib/lambdaStack.ts
@@ -113,7 +113,7 @@ export class LambdaStack extends cdk.Stack {
     // Add IAM policy to allow Lambda to manage Cognito users
     userRegistrationLambda.addToRolePolicy(
       new iam.PolicyStatement({
-        actions: ['cognito-idp:AdminCreateUser', 'cognito-idp:AdminDisableUser', 'cognito-idp:AdminDeleteUser', 'cognito-idp:ListUsers'],
+        actions: ['cognito-idp:AdminSetUserPassword', 'cognito-idp:AdminCreateUser', 'cognito-idp:AdminDisableUser', 'cognito-idp:ListUsers'],
         resources: [awsImport.cognito.userPoolArn],
       }),
     );
